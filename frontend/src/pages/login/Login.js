736110a798afaca036ba7a0b06d03b752d00bc70
SERGEY SELEZNEV <sergey_0218@mail.ru>
2021-07-06 03:47:28 +0700

    Add Formik for login page

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -1,52 +1,56 @@
-import React, { useState, Fragment } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { fetchLogin } from "../../store/api/authSlice";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 const Login = () => {
   const dispatch = useDispatch();
 
-  const [loginForm, setLoginForm] = useState({
-    email: "",
-    password: "",
+  const { handleSubmit, handleChange, values, errors } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+
+    validationSchema: yup.object({
+      email: yup.string().email("Should be valid Email").required("Email Shoud be Required"),
+      password: yup.string().required("Password Shoud be Required"),
+    }),
+
+    onSubmit: ({ email, password }) => {
+      dispatch(fetchLogin({ email, password }));
+    },
   });
 
-  const onChange = event => {
-    event.preventDefault();
-    setLoginForm({ ...loginForm, [event.target.name]: event.target.value });
-  };
-
-  const onClick = () => {
-    dispatch(fetchLogin({ email: loginForm.email, password: loginForm.password }));
-  };
-
   return (
-    <Fragment>
-      <form>
-        <h1>Авторизация</h1>
-        <input
-          placeholder="Почта"
-          type="email"
-          id="email"
-          name="email"
-          value={loginForm.email}
-          onChange={onChange}
-        />
-        <input
-          placeholder="Пароль"
-          type="password"
-          id="password"
-          name="password"
-          value={loginForm.password}
-          onChange={onChange}
-        />
-        <button type="submit" onClick={onClick}>
-          Войти
-        </button>
-        <Link to="/passwordreset">Забыли пароль?</Link>
-        <Link to="/signin">Регистрация</Link>
-      </form>
-    </Fragment>
+    <form onSubmit={handleSubmit}>
+      <h1>Авторизация</h1>
+      <input
+        placeholder="Почта"
+        type="email"
+        id="email"
+        name="email"
+        value={values.email}
+        onChange={handleChange}
+      />
+      {errors.email ? <div>{errors.email}</div> : null}
+
+      <input
+        placeholder="Пароль"
+        type="password"
+        id="password"
+        name="password"
+        value={values.password}
+        onChange={handleChange}
+      />
+      {errors.password ? <div>{errors.password}</div> : null}
+
+      <button type="submit">Войти</button>
+      <Link to="/passwordreset">Забыли пароль?</Link>
+      <Link to="/signin">Регистрация</Link>
+    </form>
   );
 };
 
