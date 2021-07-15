bd74aad3c6787185ae6b523badb1ada6ca4585bb
SERGEY SELEZNEV <sergey_0218@mail.ru>
2021-07-15 16:49:27 +0700

    Add styles for news list

--- frontend/src/components/newsList/NewsList.js
+++ frontend/src/components/newsList/NewsList.js
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchEvent } from "../../store/api/eventSlice";
 import { selectUserId } from "../../store/api/authSlice";
+import styles from "./NewsList.module.scss";
 
 const NewsList = ({ newsItems }) => {
   const dispatch = useDispatch();
@@ -13,23 +14,28 @@ const NewsList = ({ newsItems }) => {
   };
 
   return (
-    <div>
+    <div className={styles.news_section}>
       {newsItems.map(newsItem => (
-        <div key={newsItem.eventId}>
+        <div className={styles.news_item} key={newsItem.eventId}>
           <NavLink to="/event" onClick={() => onClick(newsItem.eventId)}>
-            <div>
+            <div className={styles.top_section}>
+              <div className={styles.filledbar_3}></div>
+              <div className={styles.filledbar_2}></div>
               <img src={newsItem.eventPhotoTitle} alt="No photo(" />
+              <div className={styles.filledbar_1}></div>
+              <div className={styles.filledbar_4}></div>
             </div>
           </NavLink>
-          <div>
+
+          <div className={styles.info_section}>
             <NavLink to="/event" onClick={() => onClick(newsItem.eventId)}>
               <h2>{newsItem.eventTitle}</h2>
             </NavLink>
             <p>{newsItem.eventDate + " " + newsItem.eventTime + " " + newsItem.eventLocation}</p>
-            <p>{newsItem.eventGenre}</p>
+            <p>Направление мероприятия: {newsItem.eventGenre}</p>
             <div>
-              <p>{newsItem.eventDesciption}</p>
-              <button>
+              <p className={styles.event_description}>{newsItem.eventDesciption}</p>
+              <button type="button">
                 <NavLink to="/event" onClick={() => onClick(newsItem.eventId)}>
                   Подробнее
                 </NavLink>
