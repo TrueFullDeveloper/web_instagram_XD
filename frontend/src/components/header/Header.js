5ba3005f76a8dd9162b1d7a1c9062b26506c73d3
SERGEY SELEZNEV <sergey_0218@mail.ru>
2021-07-15 17:18:36 +0700

    Add styles for header

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,14 +1,30 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styles from "./Header.module.scss";
+import logo from "../../static/images/header/logo.svg";
 
 const Header = () => {
   return (
-    <div>
-      <NavLink to="/">Главная</NavLink>
-      <NavLink to="/profile">Мой Профиль</NavLink>
-      <NavLink to="/users">Пользователи</NavLink>
-      <NavLink to="/chats">Ваши чаты</NavLink>
-      <NavLink to="/create_event">Добавить мероприятие</NavLink>
+    <div className={styles.header}>
+      <div>
+        <NavLink to="/">
+          <img src={logo} alt="nothing" />
+        </NavLink>
+      </div>
+      <div className={styles.second_section}>
+        <div className={`${styles.group_links} ${styles.first}`}>
+          <NavLink to="/users">Пользователи</NavLink>
+        </div>
+        <div className={`${styles.group_links} ${styles.second}`}>
+          <NavLink to="/chats">Ваши чаты</NavLink>
+        </div>
+        <div className={`${styles.group_links} ${styles.third}`}>
+          <NavLink to="/profile">Профиль</NavLink>
+        </div>
+        <div className={`${styles.group_links} ${styles.fourth}`}>
+          <NavLink to="/create_event">Добавить мероприятие</NavLink>
+        </div>
+      </div>
     </div>
   );
 };
