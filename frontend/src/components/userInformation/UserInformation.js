223fcbf2def0ff9d2f63ff9849d38053ffbfd30b
SERGEY SELEZNEV <sergey_0218@mail.ru>
2021-07-16 14:32:25 +0700

    Add styles for user information

--- frontend/src/components/userInformation/UserInformation.js
+++ frontend/src/components/userInformation/UserInformation.js
@@ -1,57 +1,92 @@
 import React, { useState } from "react";
 import SendMessageForm from "../sendMessageForm";
+import styles from "./UserInformation.module.scss";
+//Icons
+import emailIcon from "../../static/images/contactIcon/email.svg";
+import phoneIcon from "../../static/images/contactIcon/phone.svg";
+import instagramIcon from "../../static/images/contactIcon/instagram.svg";
+import facebookIcon from "../../static/images/contactIcon/facebook.svg";
+import vkIcon from "../../static/images/contactIcon/vk.svg";
 
-const UserInfomation = ({ userData }) => {
+const UserInformation = ({ userData }) => {
   const [messageFormIsOpen, setMessageForm] = useState(false);
 
   return (
-    <div>
-      <h1>{userData.userName}</h1>
-      <img src={userData.userPhoto} />
-      <br />
-      <h2>Email:</h2>
-      <p>{userData.email}</p>
-      <h2>Обо мне</h2>
-      <p>{userData.userInformation}</p>
-      <h2>Контакты:</h2>
+    <div className={styles.user}>
+      <div className={styles.about_section}>
+        <img src={userData.userPhoto} />
+        <h1>{userData.userName}</h1>
+        <h2>Обо мне</h2>
+        <p>{userData.userInformation}</p>
+      </div>
 
-      {userData.phoneNumber && (
+      <div className={styles.contact_section}>
+        <h2>Мои Контакты:</h2>
+
         <div>
-          <h2>Телефон</h2>
-          <p>{userData.phoneNumber}</p>
+          <span>
+            <img src={emailIcon} />
+            Email:
+          </span>
+          <p>{userData.email}</p>
         </div>
-      )}
 
-      {userData.instagram && (
-        <div>
-          <h2>Instagram</h2>
-          <p>{userData.instagram}</p>
-        </div>
-      )}
+        {userData.phoneNumber ? (
+          <div className={styles.contact_item}>
+            <span>
+              <img src={phoneIcon} />
+              Телефон
+            </span>
+            <p>{userData.phoneNumber}</p>
+          </div>
+        ) : null}
 
-      {userData.facebook && (
-        <div>
-          <h2>Facebook</h2>
-          <p>{userData.facebook}</p>
-        </div>
-      )}
+        {userData.instagram ? (
+          <div className={styles.contact_item}>
+            <span>
+              <img src={instagramIcon} />
+              Instagram
+            </span>
+            <div>
+              <a href={userData.instagram}>Мой Instagram</a>
+            </div>
+          </div>
+        ) : null}
 
-      {userData.vkontacte && (
-        <div>
-          <h2>Vk</h2>
-          <p>{userData.vkontacte}</p>
-        </div>
-      )}
+        {userData.facebook ? (
+          <div className={styles.contact_item}>
+            <span>
+              <img src={facebookIcon} />
+              Facebook
+            </span>
+            <div>
+              <a href={userData.facebook}>Мой Facebook</a>
+            </div>
+          </div>
+        ) : null}
 
-      {messageFormIsOpen ? (
-        <SendMessageForm setMessageForm={setMessageForm} userId={userData.userId} />
-      ) : (
-        <button type="button" onClick={() => setMessageForm(true)}>
-          Написать сообщение
-        </button>
-      )}
+        {userData.vkontacte ? (
+          <div className={styles.contact_item}>
+            <span>
+              <img src={vkIcon} />
+              Vk
+            </span>
+            <div>
+              <a href={userData.vkontacte}>Мой Vk</a>
+            </div>
+          </div>
+        ) : null}
+
+        {messageFormIsOpen ? (
+          <SendMessageForm setMessageForm={setMessageForm} userId={userData.userId} />
+        ) : (
+          <button type="button" onClick={() => setMessageForm(true)}>
+            Написать сообщение
+          </button>
+        )}
+      </div>
     </div>
   );
 };
 
-export default UserInfomation;
+export default UserInformation;
