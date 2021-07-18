a4c678b606ee04463029c7dfbc211e6eaa6956f5
SERGEY SELEZNEV <sergey_0218@mail.ru>
2021-07-19 00:23:29 +0700

    Add styles for feedback form

--- frontend/src/components/feedbackForm/FeedbackForm.js
+++ frontend/src/components/feedbackForm/FeedbackForm.js
@@ -5,15 +5,24 @@ import { Picker } from "emoji-mart";
 import { useDispatch, useSelector } from "react-redux";
 import { selectUserId } from "../../store/api/authSlice";
 import { addFeedback, selectEventId } from "../../store/api/eventSlice";
+import styles from "./FeedbackForm.module.scss";
 import "emoji-mart/css/emoji-mart.css";
+//Icon
+import smileIcon from "../../static/images/messageIcon/smile.svg";
 
 const FeedbackForm = () => {
   const dispatch = useDispatch();
   const userId = useSelector(selectUserId);
   const eventId = useSelector(selectEventId);
   const [pickerIsOpen, setPicker] = useState(false);
+  const [isRate, setRate] = useState(false);
 
-  const { handleSubmit, handleChange, setFieldValue, values, errors } = useFormik({
+  const onClick = userRating => {
+    setFieldValue("userRating", userRating);
+    setRate(true);
+  };
+
+  const { handleSubmit, handleChange, setFieldValue, values, errors, touched } = useFormik({
     initialValues: {
       userRating: "",
       feedbackText: "",
@@ -33,50 +42,68 @@ const FeedbackForm = () => {
   });
 
   return (
-    <form onSubmit={handleSubmit}>
-      <span>Ваша оценка</span>
-      <div>
-        <button type="button" onClick={() => setFieldValue("userRating", 1)}>
-          1
-        </button>
-        <button type="button" onClick={() => setFieldValue("userRating", 2)}>
-          2
-        </button>
-        <button type="button" onClick={() => setFieldValue("userRating", 3)}>
-          3
-        </button>
-        <button type="button" onClick={() => setFieldValue("userRating", 4)}>
-          4
-        </button>
-        <button type="button" onClick={() => setFieldValue("userRating", 5)}>
-          5
-        </button>
-      </div>
-      {errors.userRating ? <div>{errors.userRating}</div> : null}
+    <div className={styles.feedback_section}>
+      <div className={styles.border}></div>
+      <h1>Оставьте отзыв</h1>
+      <form className={styles.feedback_form} onSubmit={handleSubmit}>
+        <div className={styles.reviews}>
+          {isRate ? (
+            <span>Ваша оценка: {values.userRating}</span>
+          ) : (
+            <>
+              <i className={`ion-star ${styles.s1}`} onClick={() => onClick(5)}></i>
+              <i className={`ion-star ${styles.s2}`} onClick={() => onClick(4)}></i>
+              <i className={`ion-star ${styles.s3}`} onClick={() => onClick(3)}></i>
+              <i className={`ion-star ${styles.s4}`} onClick={() => onClick(2)}></i>
+              <i className={`ion-star ${styles.s5}`} onClick={() => onClick(1)}></i>
+            </>
+          )}
+        </div>
 
-      <span>Ваш отзыв</span>
-      <textarea
-        type="text"
-        id="feedbackText"
-        name="feedbackText"
-        value={values.feedbackText}
-        onChange={handleChange}
-      />
-
-      {pickerIsOpen && (
-        <Picker
-          set="apple"
-          onSelect={emoji => setFieldValue("feedbackText", `${values.feedbackText}${emoji.native}`)}
-          emojiSize={20}
+        <textarea
+          className={styles.feedback_form_text}
+          type="text"
+          id="feedbackText"
+          name="feedbackText"
+          placeholder="Оставьте отзыв"
+          value={values.feedbackText}
+          onChange={handleChange}
         />
-      )}
-      <button type="button" onClick={() => setPicker(!pickerIsOpen)}>
-        Смайлы
-      </button>
-      {errors.feedbackText ? <div>{errors.feedbackText}</div> : null}
 
-      <button type="submit">Оставить отзыв</button>
-    </form>
+        {touched.feedbackText && errors.feedbackText ? (
+          <span className={styles.error_message}>{errors.feedbackText}</span>
+        ) : null}
+
+        {touched.userRating && errors.userRating ? (
+          <span className={styles.error_message}>{errors.userRating}</span>
+        ) : null}
+
+        {pickerIsOpen && (
+          <div className={styles.picker}>
+            <Picker
+              set="apple"
+              onSelect={emoji => {
+                setFieldValue("feedbackText", `${values.feedbackText}${emoji.native}`);
+                setPicker(false);
+              }}
+              emojiSize={20}
+            />
+          </div>
+        )}
+
+        <button
+          className={styles.picker_button}
+          type="button"
+          onClick={() => setPicker(!pickerIsOpen)}
+        >
+          <img src={smileIcon} alt="Смайлы" />
+        </button>
+
+        <button className={styles.feedback_form_button} type="submit">
+          Оставить отзыв
+        </button>
+      </form>
+    </div>
   );
 };
 
