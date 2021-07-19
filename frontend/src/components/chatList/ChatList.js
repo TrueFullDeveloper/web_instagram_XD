a25614f51a846df6db0cf4351cf8be52d0793811
SERGEY SELEZNEV <sergey_0218@mail.ru>
2021-07-19 14:23:56 +0700

    Add styles for chat list

--- frontend/src/components/chatList/ChatList.js
+++ frontend/src/components/chatList/ChatList.js
@@ -1,22 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-//import { useDispatch } from "react-redux";
-//TODO: Fix NavLink (search string should be look like this: "chatroom2412")
+import styles from "./ChatList.module.scss";
 
 const ChatList = ({ chatList }) => {
   return (
-    <div>
-      <h2>Список ваших чатов:</h2>
+    <div className={styles.chat_list_section}>
+      <h1>Список ваших чатов:</h1>
 
       {chatList.map(chatListItem => (
-        <NavLink to="/chatroom" key={chatListItem.chatId}>
-          <div>
-            <h1>{chatListItem.companion.userName}</h1>
+        <div className={styles.chat_room_box}>
+          <NavLink to="/chatroom" key={chatListItem.chatId}>
             <div>
               <img src={chatListItem.companion.userPhoto} alt="No photo(" />
             </div>
-          </div>
-        </NavLink>
+
+            <div>
+              <h2>{chatListItem.companion.userName}</h2>
+            </div>
+          </NavLink>
+        </div>
       ))}
     </div>
   );
