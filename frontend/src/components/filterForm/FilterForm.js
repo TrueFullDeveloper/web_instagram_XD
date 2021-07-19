82ad5ed6eba74e7c07bbfefbccfbebea38ab38c1
SERGEY SELEZNEV <sergey_0218@mail.ru>
2021-07-19 15:05:54 +0700

    Add styles for filter form

--- frontend/src/components/filterForm/FilterForm.js
+++ frontend/src/components/filterForm/FilterForm.js
@@ -6,6 +6,7 @@ import Select from "react-select";
 import Calendar from "react-calendar";
 import { searchEvent } from "../../store/api/newsSlice";
 import "react-calendar/dist/Calendar.css";
+import styles from "./FilterForm.module.scss";
 
 // TODO: Transfer it to Config
 const configLocationOptions = [
@@ -25,6 +26,10 @@ const configIntervalOptions = [
   { value: "past", label: "Прошедшие" },
   { value: "all", label: "Все" },
 ];
+
+const selectStyles = {
+  control: css => ({ ...css, marginTop: "20px", height: "60px" }),
+};
 
 const FilterForm = () => {
   const [calendarIsOpen, setCalendarOpen] = useState(false);
@@ -49,72 +54,101 @@ const FilterForm = () => {
   });
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <input
-          type="search"
-          name="userQuery"
-          id="userQuery"
-          placeholder="Поиск мероприятия"
-          value={values.userQuery}
-          onChange={handleChange}
-        />
-        <button type="submit">Поиск</button>
+    <div className={styles.filter_form}>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            className={styles.search_field}
+            type="search"
+            name="userQuery"
+            id="userQuery"
+            placeholder="Поиск мероприятия"
+            value={values.userQuery}
+            onChange={handleChange}
+          />
+          <button type="submit" className={styles.search_button}>
+            Поиск
+          </button>
 
-        {errors.userQuery ? <div>{errors.userQuery}</div> : null}
+          {errors.userQuery ? <div>{errors.userQuery}</div> : null}
 
-        <Select
-          name="interval"
-          id="interval"
-          options={configIntervalOptions}
-          placeholder="Выберите временной промежуток"
-          onChange={event => setFieldValue("interval", event.value)}
-        />
+          <Select
+            name="interval"
+            id="interval"
+            options={configIntervalOptions}
+            placeholder="Выберите временной промежуток"
+            onChange={event => setFieldValue("interval", event.value)}
+            styles={selectStyles}
+          />
 
-        <Select
-          name="location"
-          id="location"
-          options={configLocationOptions}
-          placeholder="Выберите город"
-          onChange={event => setFieldValue("location", event.value)}
-          isSearchable
-        />
+          <Select
+            name="location"
+            id="location"
+            options={configLocationOptions}
+            placeholder="Выберите город"
+            onChange={event => setFieldValue("location", event.value)}
+            styles={selectStyles}
+            isSearchable
+          />
 
-        <Select
-          name="genre"
-          id="genre"
-          options={configGenreOptions}
-          onChange={event =>
-            setFieldValue(
-              "genre",
-              event.map(eventItem => eventItem.value)
-            )
-          }
-          placeholder="Выберите направление мероприятия"
-          isSearchable
-          isMulti
-        />
+          <Select
+            name="genre"
+            id="genre"
+            options={configGenreOptions}
+            onChange={event =>
+              setFieldValue(
+                "genre",
+                event.map(eventItem => eventItem.value)
+              )
+            }
+            styles={selectStyles}
+            placeholder="Выберите направление мероприятия"
+            isSearchable
+            isMulti
+          />
 
-        {calendarIsOpen ? (
-          <>
-            <span>Дата: {values.eventDate}</span>
-            <Calendar
-              name="eventDate"
-              id="eventDate"
-              onChange={event =>
-                setFieldValue(
-                  "eventDate",
-                  `${event.getDate()}.${event.getMonth()}.${event.getFullYear()}`
-                )
-              }
-            />
-            <button onClick={() => setCalendarOpen(false)}>Закрыть</button>
-          </>
-        ) : (
-          <button onClick={() => setCalendarOpen(true)}>Выбать дату</button>
-        )}
-      </div>
-    </form>
+          {calendarIsOpen ? (
+            <>
+              <Calendar
+                name="eventDate"
+                id="eventDate"
+                onChange={event => {
+                  setFieldValue(
+                    "eventDate",
+                    `${event.getDate()}.${event.getMonth()}.${event.getFullYear()}`
+                  );
+                  setCalendarOpen(false);
+                }}
+                styles={selectStyles}
+              />
+              <button
+                className={`${styles.calendar_button} ${styles.close_button}`}
+                type="button"
+                onClick={() => setCalendarOpen(false)}
+              >
+                Закрыть
+              </button>
+            </>
+          ) : (
+            <>
+              {values.eventDate ? (
+                <span>Дата: {values.eventDate}</span>
+              ) : (
+                <span>Дата не выбрана</span>
+              )}
+
+              <button
+                className={styles.calendar_button}
+                type="button"
+                onClick={() => setCalendarOpen(true)}
+              >
+                Выбать дату
+              </button>
+            </>
+          )}
+        </div>
+      </form>
+    </div>
   );
 };
 
