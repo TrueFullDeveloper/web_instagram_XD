edc11efcfbaf39bc160695cf521f0282973f7ff1
Seleznev Sergey <seleznevsa@aldera.loc>
2021-07-23 16:13:54 +0700

    Add translation to chat page

--- frontend/src/components/chatList/ChatList.js
+++ frontend/src/components/chatList/ChatList.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./ChatList.module.scss";
+//i18n
+import { useTranslation } from "react-i18next";
 
 const ChatList = ({ chatList }) => {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.chat_list_section}>
-      <h1>Список ваших чатов:</h1>
+      <h1>{t("pages.chatPage.userChatList")}</h1>
 
       {chatList.map(chatListItem => (
         <div className={styles.chat_room_box}>
