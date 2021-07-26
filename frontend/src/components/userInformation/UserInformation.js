c4e9929bb732501624eb8a5fbbe924e887537fa7
Seleznev Sergey <seleznevsa@aldera.loc>
2021-07-26 11:16:13 +0700

    Add translation to user page

--- frontend/src/components/userInformation/UserInformation.js
+++ frontend/src/components/userInformation/UserInformation.js
@@ -7,8 +7,11 @@ import phoneIcon from "../../static/images/contactIcon/phone.svg";
 import instagramIcon from "../../static/images/contactIcon/instagram.svg";
 import facebookIcon from "../../static/images/contactIcon/facebook.svg";
 import vkIcon from "../../static/images/contactIcon/vk.svg";
+//i18n
+import { useTranslation } from "react-i18next";
 
 const UserInformation = ({ userData }) => {
+  const { t } = useTranslation();
   const [messageFormIsOpen, setMessageForm] = useState(false);
 
   return (
@@ -16,12 +19,12 @@ const UserInformation = ({ userData }) => {
       <div className={styles.about_section}>
         <img src={userData.userPhoto} alt="" />
         <h1>{userData.userName}</h1>
-        <h2>Обо мне</h2>
+        <h2>{t("pages.user.aboutTitle")}</h2>
         <p>{userData.userInformation}</p>
       </div>
 
       <div className={styles.contact_section}>
-        <h2>Мои Контакты:</h2>
+        <h2>{t("pages.user.contactSectionTitle")}</h2>
 
         <div>
           <span>
@@ -35,7 +38,7 @@ const UserInformation = ({ userData }) => {
           <div className={styles.contact_item}>
             <span>
               <img src={phoneIcon} alt="" />
-              Телефон
+              {t("pages.user.userPhoneTitle")}
             </span>
             <p>{userData.phoneNumber}</p>
           </div>
@@ -48,7 +51,7 @@ const UserInformation = ({ userData }) => {
               Instagram
             </span>
             <div>
-              <a href={userData.instagram}>Мой Instagram</a>
+              <a href={userData.instagram}>{t("pages.user.userInstagramLink")}</a>
             </div>
           </div>
         ) : null}
@@ -60,7 +63,7 @@ const UserInformation = ({ userData }) => {
               Facebook
             </span>
             <div>
-              <a href={userData.facebook}>Мой Facebook</a>
+              <a href={userData.facebook}>{t("pages.user.userFacebookLink")}</a>
             </div>
           </div>
         ) : null}
@@ -72,7 +75,7 @@ const UserInformation = ({ userData }) => {
               Vk
             </span>
             <div>
-              <a href={userData.vkontacte}>Мой Vk</a>
+              <a href={userData.vkontacte}>{t("pages.user.userVkontakteLink")}</a>
             </div>
           </div>
         ) : null}
@@ -81,7 +84,7 @@ const UserInformation = ({ userData }) => {
           <SendMessageForm setMessageForm={setMessageForm} userId={userData.userId} />
         ) : (
           <button type="button" onClick={() => setMessageForm(true)}>
-            Написать сообщение
+            {t("pages.user.sendMessageButton")}
           </button>
         )}
       </div>
